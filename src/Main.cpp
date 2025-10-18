d64245f1033219c63ab876b398c2f7a3ad2cc86f
Mark Tabatskyi <mark.tabatskyi@gmail.com>
2025-10-18 14:56:49 +0300

    added movement

--- src/Main.cpp
+++ src/Main.cpp
@@ -41,34 +41,55 @@ int main(int argc, char* argv[])
         {
             SDL_Rect squareRect;
 
-            squareRect.w = std::min(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
-            squareRect.h = std::min(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
+            squareRect.w = std::min(SCREEN_WIDTH, SCREEN_HEIGHT) / 10;
+            squareRect.h = std::min(SCREEN_WIDTH, SCREEN_HEIGHT) / 10;
 
             squareRect.x = SCREEN_WIDTH / 2 - squareRect.w / 2;
             squareRect.y = SCREEN_HEIGHT / 2 - squareRect.h / 2;
 
             bool quit = false;
+            const int speed = 5; 
 
             while (!quit)
             {
                 SDL_Event e;
 
-                SDL_WaitEvent(&e);
-
-                if (e.type == SDL_QUIT)
+                while (SDL_PollEvent(&e))
                 {
-                    quit = true;
+                    if (e.type == SDL_QUIT)
+                    {
+                        quit = true;
+                    }
                 }
 
-                SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+                const Uint8* state = SDL_GetKeyboardState(nullptr);
+                int dx = 0;
+                int dy = 0;
 
+                if (state[SDL_SCANCODE_LEFT] || state[SDL_SCANCODE_A])
+                    dx -= speed;
+                if (state[SDL_SCANCODE_RIGHT] || state[SDL_SCANCODE_D])
+                    dx += speed;
+                if (state[SDL_SCANCODE_UP] || state[SDL_SCANCODE_W])
+                    dy -= speed;
+                if (state[SDL_SCANCODE_DOWN] || state[SDL_SCANCODE_S])
+                    dy += speed;
+
+                squareRect.x += dx;
+                squareRect.y += dy;
+
+                squareRect.x = std::max(0, std::min(static_cast<int>(SCREEN_WIDTH) - static_cast<int>(squareRect.w), squareRect.x));
+                squareRect.y = std::max(0, std::min(static_cast<int>(SCREEN_HEIGHT) - static_cast<int>(squareRect.h), squareRect.y));
+
+                SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
                 SDL_RenderClear(renderer);
 
                 SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
-
                 SDL_RenderFillRect(renderer, &squareRect);
 
                 SDL_RenderPresent(renderer);
+
+                SDL_Delay(16);
             }
 
             SDL_DestroyRenderer(renderer);
