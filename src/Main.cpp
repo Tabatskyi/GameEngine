44a6cdda4cdfd449b3673026573418656445cbf2
Mark Tabatskyi <mark.tabatskyi@gmail.com>
2025-10-18 14:46:32 +0300

    created game loop

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,60 +1,83 @@
 #include <SDL2/SDL.h>
 #include <glm/trigonometric.hpp>
 #include <iostream>
+#include <algorithm>
 
-const unsigned int SCREEN_WIDTH = 640;
-const unsigned int SCREEN_HEIGHT = 480;
+constexpr const unsigned int SCREEN_WIDTH = 800;
+constexpr const unsigned int SCREEN_HEIGHT = 600;
 
 int main(int argc, char* argv[])
 {
-	std::cout << "cos(60 degrees) = " << glm::cos(glm::radians(60.0f)) << std::endl;
+    (void)argc;
+    (void)argv;
 
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
-		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-		return 1;
-	}
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cout << "SDL could not be initialized!" << std::endl
+            << "SDL_Error: " << SDL_GetError() << std::endl;
+        return 0;
+    }
 
-	SDL_Window* window = SDL_CreateWindow(
-		"SDL Window",
-		SDL_WINDOWPOS_CENTERED,
-		SDL_WINDOWPOS_CENTERED,
-		SCREEN_WIDTH,
-		SCREEN_HEIGHT,
-		SDL_WINDOW_SHOWN);
+    SDL_Window* window = SDL_CreateWindow(
+        "SDL Window",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        SCREEN_WIDTH, SCREEN_HEIGHT,
+        SDL_WINDOW_SHOWN);
 
-	if (window == NULL)
-	{
-		std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
-		SDL_Quit();
-		return 1;
-	}
+    if (!window)
+    {
+        std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+    }
+    else
+    {
+        SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+        if (!renderer)
+        {
+            std::cout << "Renderer could not be created!" << std::endl
+                << "SDL_Error: " << SDL_GetError() << std::endl;
+        }
+        else
+        {
+            SDL_Rect squareRect;
 
-	SDL_Surface* screenSurface = SDL_GetWindowSurface(window);
-	if (screenSurface == NULL)
-	{
-		std::cout << "Could not get window surface! SDL_Error: " << SDL_GetError() << std::endl;
-		SDL_DestroyWindow(window);
-		SDL_Quit();
-		return 1;
-	}
+            squareRect.w = std::min(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
+            squareRect.h = std::min(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
 
-	SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-	SDL_UpdateWindowSurface(window);
+            squareRect.x = SCREEN_WIDTH / 2 - squareRect.w / 2;
+            squareRect.y = SCREEN_HEIGHT / 2 - squareRect.h / 2;
 
-	bool quit = false;
-	SDL_Event e;
-	while (!quit)
-	{
-		while (SDL_PollEvent(&e))
-		{
-			if (e.type == SDL_QUIT)
-				quit = true;
-		}
-		SDL_Delay(10);
-	}
+            bool quit = false;
 
-	SDL_DestroyWindow(window);
-	SDL_Quit();
-	return 0;
+            while (!quit)
+            {
+                SDL_Event e;
+
+                SDL_WaitEvent(&e);
+
+                if (e.type == SDL_QUIT)
+                {
+                    quit = true;
+                }
+
+                SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+                SDL_RenderClear(renderer);
+
+                SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
+
+                SDL_RenderFillRect(renderer, &squareRect);
+
+                SDL_RenderPresent(renderer);
+            }
+
+            SDL_DestroyRenderer(renderer);
+        }
+
+        SDL_DestroyWindow(window);
+    }
+
+    SDL_Quit();
+
+    return 0;
 }
